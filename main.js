59c3d991ecae15e777fed74927ffe6433efe8da7
EFREN NARVAEZ <efrennm86@gmail.com>
2023-09-03 19:16:55 -0600

    Correcciones finales

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ let CVVNumber =[];
 
 inputCard.addEventListener("keydown", (e) => {
     if(e.key=='Tab'){
-        return;
+        return; //Realmente no hace nada
     }
 
-    e.preventDefault();
-    handleInput(maskNumber, e.key, cardNumber);
-    inputCard.value = cardNumber.join("");
+    e.preventDefault(); //Prevenimos cualquier comportamiento normal 
+    handleInput(maskNumber, e.key, cardNumber); //Función para el procesamiento de los datos
+    inputCard.value = cardNumber.join(""); //Convertimos los elementos del arreglo cardNumber en un string el cual se asigna en el input
 }); 
 
 inputDate.addEventListener("keydown", (e) => {
@@ -28,7 +28,7 @@ inputDate.addEventListener("keydown", (e) => {
 
     e.preventDefault();
     handleInput(maskDate, e.key, dateNumber);
-    inputDate.value = cardDate.join("");
+    inputDate.value = dateNumber.join("");
 });
 
 inputCVV.addEventListener("keydown", (e) => {
@@ -42,6 +42,7 @@ inputCVV.addEventListener("keydown", (e) => {
 });
 
 function handleInput(mask, key, arr){
+    //Elementos permitidos para su procesamiento. Los demás serán ignorados
     let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     if(key =='Backspace' && arr.length>0 ){
         arr.pop();
